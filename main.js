645e6a3e375b4a58333da789a500e5b50db425cc
Braulio <brauliortegabatalla@gmail.com>
2025-04-22 00:34:01 -0500

    'Terminado'

---
+++ main.js
@@ -0,0 +1,8 @@
+let boton = document.querySelector('.boton')
+let cardFooter = document.querySelector('.card-footer')
+boton.addEventListener('click',activar)
+
+function activar() {
+    cardFooter.classList.toggle('activo');
+}
+console.log('xd')
